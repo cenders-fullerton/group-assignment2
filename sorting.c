06936f96fd7672f8f475ae719014aaca080ed0b7
Caleb Enders <cenders@csu.fullerton.edu>
2024-10-04 22:04:56 -0700

    added bare bones thread code, confusion in how exactly passing an array into a function works

--- sorting.c
+++ sorting.c
@@ -3,20 +3,26 @@
 #include <stdlib.h>
 
 #define SIZE (sizeof(list)/sizeof(*list))
-int list[10] = {7,12,19,3,18,4,2,6,15,8};
+int list[] = {7,12,19,3,18,4,2,6,15,8};
 int result[SIZE] = {0};
 
 int compare(const void* a, const void* b) {
   return(*(int*)a - *(int*)b);
 }
-
+void* Sorting(int x[],int size){
+	//sleep(1);
+   int n = size/sizeof(x[0]);
+ 
+   qsort(list, n, sizeof(int), compare);
+   
+   for (int i = 0; i < n; i++){
+    printf("%d ",x[i]);
+   }
+   return NULL;
+	}
 int main() {
-  int n = sizeof(list)/sizeof(list[0]);
- 
-  qsort(list, n, sizeof(int), compare);
- 
-  for (int i = 0; i < n; i++){
-    printf("%d",list[i]);
-  }
+	pthread_t thread_id;
+	pthread_create(&thread_id,NULL,Sorting,NULL);
+	pthread_join(thread_id,NULL);
   return 0;
 }
